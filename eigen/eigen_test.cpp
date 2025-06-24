77615506c9df3a9a6e6d3309bc66500a681cb22f
Brandon <brandonamorgan@gmail.com>
2025-06-24 16:28:50 -0700

    Started using Eigen library

---
+++ eigen/eigen_test.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Brandon on 6/24/2025.
+//
+#include "eigen_test.h"
+#include <Eigen/Core>
+#include <Eigen/Dense>
+
+using namespace Eigen;
+
+void eigen_test(){
+Matrix<float, 3, 1> vd_d3;
+
+}
---
+++ eigen/eigen_test.h
@@ -0,0 +1,13 @@
+//
+// Created by Brandon on 6/24/2025.
+//
+
+#ifndef EIGEN_TEST_H
+#define EIGEN_TEST_H
+
+
+void eigen_test();
+
+
+
+#endif //EIGEN_TEST_H
